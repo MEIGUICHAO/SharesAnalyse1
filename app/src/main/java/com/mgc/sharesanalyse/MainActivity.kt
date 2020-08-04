c04ea08e44c53026ba51fef9120f9090945d4b60
莫贵朝 <499976081@qq.com>
2020-08-04 17:56:29 +0800

    net success

--- app/src/main/java/com/mgc/sharesanalyse/MainActivity.kt
+++ app/src/main/java/com/mgc/sharesanalyse/MainActivity.kt
@@ -3,75 +3,41 @@ package com.mgc.sharesanalyse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.galanz.rxretrofit.network.RetrofitManager
 import com.mgc.sharesanalyse.base.Datas
+import com.mgc.sharesanalyse.net.LoadState
 import com.mgc.sharesanalyse.utils.LogUtil
+import com.mgc.sharesanalyse.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
-    val urlArray = arrayOfNulls<String>(10)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.loadState.observe(this, Observer {
+            when (it) {
+                is LoadState.Success->  {
+                    viewModel.sharesDats
+                }
+                is LoadState.Fail ->  {
+
+                }
+                is LoadState.Loading ->  {
+
+                }
+            }
+        })
         btnRequest.setOnClickListener{
-            requestData()
+            viewModel.requestData()
         }
     }
 
-    private fun requestData() {
-        for (index in 0..9) {
-            urlArray[index] = null
-        }
-        val splitArray = Datas.sharesList.split(")")
-        Log.d("mgc", "splitArray size:${splitArray.size}")
-
-        for (index in 0..splitArray.size) {
-            if (index < 1000) {
-                val keepDigital =
-                    (if (urlArray[index / 100].isNullOrEmpty()) "sh" else ",sh") + keepDigital(
-                        splitArray[index]
-                    )
-                Log.d("mgc", "keepDigital:$keepDigital")
-                urlArray[index / 100] =
-                    (if (urlArray[index / 100].isNullOrEmpty()) "" else urlArray[index / 100]) + keepDigital
-            }
-        }
-
-        var resultArray = arrayOfNulls<Deferred<String>>(10)
-        var index = 0
-        GlobalScope.launch(Dispatchers.IO) {
-            urlArray.forEach {
-                it?.let {
-                    LogUtil.d("mgc", "!!!!index$index,url:$it")
-                    resultArray[index] = RetrofitManager.reqApi.getSharesDatas(it)
-                    index++
-                }
-            }
-            index = 0
-            resultArray.forEach {
-                var result = it!!.await()
-                val split = result.split(";")
-                LogUtil.d("mgc", "!!!index:$index,result size:${split.size}")
-                split.forEach{
-                    LogUtil.d("result:$it")
-                }
-                index++
-            }
-
-        }
-    }
-
-
-    fun keepDigital(oldString: String): String {
-        val newString = StringBuffer()
-        val matcher = Pattern.compile("\\d").matcher(oldString)
-        while (matcher.find()) {
-            newString.append(matcher.group())
-        }
-        return newString.toString()
-    }
 }
---
+++ app/src/main/java/com/mgc/sharesanalyse/viewmodel/MainViewModel.kt
@@ -0,0 +1,80 @@
+package com.mgc.sharesanalyse.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.galanz.rxretrofit.network.RetrofitManager
+import com.mgc.sharesanalyse.base.Datas
+import com.mgc.sharesanalyse.net.LoadState
+import com.mgc.sharesanalyse.utils.LogUtil
+import kotlinx.coroutines.*
+import java.util.regex.Pattern
+
+
+class MainViewModel : ViewModel() {
+    val sharesDats = MutableLiveData<List<String>>()
+    val loadState = MutableLiveData<LoadState>()
+
+
+    fun requestData() {
+        val urlArray = arrayOfNulls<String>(10)
+        val splitArray = Datas.sharesList.split(")")
+        Log.d("mgc", "splitArray size:${splitArray.size}")
+
+        for (index in 0..splitArray.size) {
+            if (index < 1000) {
+                val keepDigital =
+                    (if (urlArray[index / 100].isNullOrEmpty()) "sh" else ",sh") + keepDigital(
+                        splitArray[index]
+                    )
+                Log.d("mgc", "keepDigital:$keepDigital")
+                urlArray[index / 100] =
+                    (if (urlArray[index / 100].isNullOrEmpty()) "" else urlArray[index / 100]) + keepDigital
+            }
+        }
+
+        var resultArray = arrayOfNulls<Deferred<String>>(10)
+        var index = 0
+
+        launch({
+            loadState.value = LoadState.Loading()
+            urlArray.forEach {
+                it?.let {
+                    LogUtil.d("mgc", "!!!!index$index,url:$it")
+                    resultArray[index] = RetrofitManager.reqApi.getSharesDatas(it)
+                    index++
+                }
+            }
+            index = 0
+            resultArray.forEach {
+                it?.let {
+                    var result = it.await()
+                    val split = result.split(";")
+                    LogUtil.d("mgc", "!!!index:$index,result size:${split.size}")
+                    split.forEach{
+                        LogUtil.d("result:$it")
+                    }
+                }
+                index++
+            }
+            loadState.value = LoadState.Success()
+
+        },{
+            loadState.value = LoadState.Fail(it.message ?: "加载失败")
+
+        })
+
+    }
+
+
+    fun keepDigital(oldString: String): String {
+        val newString = StringBuffer()
+        val matcher = Pattern.compile("\\d").matcher(oldString)
+        while (matcher.find()) {
+            newString.append(matcher.group())
+        }
+        return newString.toString()
+    }
+
+}
