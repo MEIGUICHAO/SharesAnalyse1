83bbcb0a24d9641095dd8d9a701e8b31514a3b28
莫贵朝 <499976081@qq.com>
2020-08-15 17:49:11 +0800

    avalible

--- app/src/main/java/com/mgc/sharesanalyse/MainActivity.kt
+++ app/src/main/java/com/mgc/sharesanalyse/MainActivity.kt
@@ -179,7 +179,7 @@ class MainActivity : AppCompatActivity() {
                 logBySplite(it.gt100times, tag, "ps_gt100times")
             }
             if (logStrList.size >= Datas.limitSize || !needJudeSize) {
-                Log.d(tag, "----code:${code}-----name:$name-----size:${logStrList.size}-----close prices-----:${lastBean.current}!!!")
+                Log.d(tag, "---code:${code}---name:$name---size:${logStrList.size}---open:${lastBean.open}---close:${lastBean.current}!!!")
                 logStrList.forEach {
                     Log.d(tag, it)
                 }
